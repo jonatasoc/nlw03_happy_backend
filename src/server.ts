3d90a12892b043d4f2fd3cee17b264fc3af5510d
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-10-15 15:44:02 -0300

    Creating .sqlite database. Creating first migration to create Orphanages Table

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import './database';
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  res.json({message: "Hello World!"});
+})
 
 app.listen(3333, () => {
   console.log("Server Started!");
