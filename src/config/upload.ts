a98f0858f6da1e1361b181c55bd3c0da174c5813
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-10-17 12:23:15 -0300

    Serving upload folder

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,8 +1,6 @@
 import path from 'path';
 import multer from 'multer';
 
-const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
-
 export default {
   directory: tempFolder,
   storage: multer.diskStorage({
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import './database';
 import routes from './routes';
 
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploados')));
 
 app.listen(3333, () => {
   console.log("Server Started!");
