2a719cfaeef813ba9b5f7debf156baca7393d137
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-10-29 17:54:38 -0300

    Adding morgan logs

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import path from 'path';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import './database';
 import routes from './routes';
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
